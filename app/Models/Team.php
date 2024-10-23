957d4de75e11fffe78254634f0b2a503a2f9e71a
Guilherme Mendes <guilherme8787@gmail.com>
2024-10-23 10:24:43 -0300

    feat: add models for new tables

---
+++ app/Models/Team.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'conference',
+        'division',
+        'city',
+        'name',
+        'full_name',
+        'abbreviation'
+    ];
+
+    public function players()
+    {
+        return $this->hasMany(Player::class);
+    }
+}
